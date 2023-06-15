78e022f7d06e1918e7c623db0da17cfe1c44031e
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-15 10:28:14 +0700

    Add project files.

---
+++ StackUsingArray_004_A/StackUsingArray_004_A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Program {
+private:
+	string stack_array[5];
+	int top;
+
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+
+	}
+};
